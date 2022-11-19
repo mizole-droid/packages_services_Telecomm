096149e9e474d56a5dcf0fda88b3eaad4a35450e
3zore <minrei728@gmail.com>
2022-11-19 21:47:53 +0900

    Revert "Telecomm: Update vibration patterns"
    This reverts commit a2f931b95d917e7c19919f94ce467a378e5ec3a6.

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -116,17 +116,15 @@ public class Ringer {
 
     private static final long[] SIMPLE_VIBRATION_PATTERN = {
         0, // No delay before starting
-        1000, // How long to vibrate
-        1000, // How long to wait before vibrating again
-        1000, // How long to vibrate
-        1000, // How long to wait before vibrating again
+        800, // How long to vibrate
+        800, // How long to wait before vibrating again
     };
 
     private static final long[] DZZZ_DA_VIBRATION_PATTERN = {
         0, // No delay before starting
         500, // How long to vibrate
         200, // Delay
-        70, // How long to vibrate
+        20, // How long to vibrate
         720, // How long to wait before vibrating again
     };
 
@@ -142,9 +140,11 @@ public class Ringer {
 
     private static final long[] DA_DA_DZZZ_VIBRATION_PATTERN = {
         0, // No delay before starting
-        70, // How long to vibrate
+        30, // How long to vibrate
         80, // Delay
-        70, // How long to vibrate
+        30, // How long to vibrate
+        80, // Delay
+        50,  // How long to vibrate
         180, // Delay
         600,  // How long to vibrate
         1050, // How long to wait before vibrating again
@@ -156,8 +156,20 @@ public class Ringer {
         200, // Delay
         600, // How long to vibrate
         150, // Delay
-        60,  // How long to vibrate
+        20,  // How long to vibrate
         1050, // How long to wait before vibrating again
+    };
+
+    private static final int[] NINE_ELEMENTS_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
+        255,
+        0,
+        255,
+        0,
+        255,
+        0,
     };
 
     private static final int[] SEVEN_ELEMENTS_VIBRATION_AMPLITUDE = {
@@ -176,6 +188,12 @@ public class Ringer {
         0, // No amplitude while waiting
         255,
         0,
+    };
+
+    private static final int[] SIMPLE_VIBRATION_AMPLITUDE = {
+        0, // No delay before starting
+        255, // Vibrate full amplitude
+        0, // No amplitude while waiting
     };
 
     private static final long[] CALL_WAITING_VIBRATION_PATTERN = {
@@ -811,7 +829,7 @@ public class Ringer {
                     break;
                 case 3:
                     mDefaultVibrationEffect = mVibrationEffectProxy.createWaveform(DA_DA_DZZZ_VIBRATION_PATTERN,
-                        SEVEN_ELEMENTS_VIBRATION_AMPLITUDE, REPEAT_SIMPLE_VIBRATION_AT);
+                        NINE_ELEMENTS_VIBRATION_AMPLITUDE, REPEAT_SIMPLE_VIBRATION_AT);
                     break;
                 case 4:
                     mDefaultVibrationEffect = mVibrationEffectProxy.createWaveform(DA_DZZZ_DA_VIBRATION_PATTERN,
@@ -845,7 +863,7 @@ public class Ringer {
                     break;
                 default:
                     mDefaultVibrationEffect = mVibrationEffectProxy.createWaveform(SIMPLE_VIBRATION_PATTERN,
-                        FIVE_ELEMENTS_VIBRATION_AMPLITUDE, REPEAT_SIMPLE_VIBRATION_AT);
+                        SIMPLE_VIBRATION_AMPLITUDE, REPEAT_SIMPLE_VIBRATION_AT);
                     break;
             }
         } else {
